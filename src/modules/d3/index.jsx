cb7307340e85b76398cae4471e0a40f9296d711f
jack <jack@asus.local>
2020-06-28 16:30:10 +0300

    added update controls

--- src/modules/d3/index.jsx
+++ src/modules/d3/index.jsx
@@ -1,19 +1,29 @@
 import React, { PureComponent } from "react";
 import { BarNameValueChart } from "./bar-name-value";
 import { getBarNameValueData } from "./bar-name-value/data";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 export class D3Charts extends PureComponent {
   state = {
+    updateTime: 2000,
     barNameValue: getBarNameValueData(),
   };
 
   componentDidMount() {
-    this.intervalRef = setInterval(this.changeData, 2000);
+    this.setInterval(this.state.updateTime);
   }
 
   componentWillUnmount() {
-    window.clearInterval(this.intervalRef);
+    this.clearInterval();
   }
+
+  clearInterval = () => {
+    window.clearInterval(this.intervalRef);
+  };
+
+  setInterval = (updateTime) => {
+    this.intervalRef = setInterval(this.changeData, updateTime);
+  };
 
   changeData = () => {
     this.setState({
@@ -21,9 +31,55 @@ export class D3Charts extends PureComponent {
     });
   };
 
+  changeUpdateTime = (updateTime) => () => {
+    this.clearInterval();
+    this.setState({ updateTime });
+
+    if (updateTime !== 0) {
+      this.setInterval(updateTime);
+    }
+  };
+
+  isActive = (time) => {
+    return this.state.updateTime === time;
+  };
+
   render() {
     return (
       <div style={{ padding: 10 }}>
+        <div style={{ margin: 10, display: "flex", alignItems: "baseline" }}>
+          <p style={{ marginRight: 10 }}>Update every:</p>
+          <ButtonGroup aria-label="Basic example">
+            <Button
+              variant="info"
+              onClick={this.changeUpdateTime(0)}
+              active={this.isActive(0)}
+            >
+              None
+            </Button>
+            <Button
+              variant="info"
+              onClick={this.changeUpdateTime(2000)}
+              active={this.isActive(2000)}
+            >
+              2s
+            </Button>
+            <Button
+              variant="info"
+              onClick={this.changeUpdateTime(5000)}
+              active={this.isActive(5000)}
+            >
+              5s
+            </Button>
+            <Button
+              variant="info"
+              onClick={this.changeUpdateTime(10000)}
+              active={this.isActive(10000)}
+            >
+              10s
+            </Button>
+          </ButtonGroup>
+        </div>
         <BarNameValueChart
           width={300}
           height={300}
