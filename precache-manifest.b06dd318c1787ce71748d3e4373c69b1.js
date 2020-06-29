4ddb7cffd2fb0930a8a871a90ccc6da0f3513107
jack <jack@asus.local>
2020-06-29 22:44:06 +0300

    Updates

---
+++ precache-manifest.b06dd318c1787ce71748d3e4373c69b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c490894b762cc1621c5d92be84c821cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a21eb05b5c8d00534de",
+    "url": "/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "b2c7f4e972780e14bc84",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a21eb05b5c8d00534de",
+    "url": "/static/js/2.60d9c131.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.60d9c131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c7f4e972780e14bc84",
+    "url": "/static/js/main.cc516e0c.chunk.js"
+  },
+  {
+    "revision": "b05cf39025c60f23c8cc",
+    "url": "/static/js/runtime-main.682430a5.js"
+  }
+]);
